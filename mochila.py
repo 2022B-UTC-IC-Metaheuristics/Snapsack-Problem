6ef811291e8d180215082b489455abd75d9a2bfa
iDiegoIbarra <56168184+iDiegoIbarra@users.noreply.github.com>
2022-03-29 22:17:00 -0600

    Add files via upload

--- mochila.py
+++ mochila.py
@@ -2,36 +2,40 @@ import math
 import random as rn
 import numpy as np
 import sys
+sys.setrecursionlimit(8000)
 T=5
-L=20
+L=5
 alpha=.55
-sAct= np.array([0,0,0,0,0])
+sAct= np.array([0,0,0,0,0,0,0,0,0,0])
 sCand=sAct
-v=np.array([20,4,25,2,8])
-p=np.array([10,2,14,8,5])
+v=np.array([20,4,5,2,8,20,4,5,2,8])
+p=np.array([10,2,14,8,5,10,2,14,8,5])
+size = v.shape[0]
 R=0.2
 cap=20
 k=1.380649*pow(10,-23)
-sys.setrecursionlimit(8000)
-def Genera_Vecino():
-    for i in range(sCand.shape[0]):
+
+def Genera_Vecino(size, cap):
+    sol = np.zeros(size)
+    for i in range(size):
         r = rn.random()
         if r > .5:
-            sCand[i] = 1
+            sol[i] = 1
         else:
-            sCand[i] = 0
+            sol[i] = 0
     c=0
-    for i in range(sCand.shape[0]):
-        if sCand[i] == 1:
+    #print("dentro fun:", sol)
+    for i in range(size):
+        if sol[i] == 1:
             c+=p[i]
     if c > cap:
-        Genera_Vecino()
-    else:
-        return
+        sol=Genera_Vecino(size,cap)
     
-def Costo(sol):
+    return sol
+    
+def Costo(sol,size):
     f=0
-    for i in range(sol.shape[0]):
+    for i in range(size):
         if sol[i] == 1:
             f+=v[i]
     return f
@@ -39,8 +43,8 @@ def Costo(sol):
 def recocido(sAct,T,L,alpha,R):
     while T > 0.001 :
         for i in range(L):
-            Genera_Vecino()
-            AE=Costo(sCand) - Costo(sAct)
+            sCand = Genera_Vecino(size,cap)
+            AE=Costo(sCand,size) - Costo(sAct,size)
             if AE >= 0:
                 sAct = sCand
             else:
